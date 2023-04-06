11a27d87848a66b77dece42094079d784060629d
muflikhandimasd <muflikhan.dimas@gmail.com>
2023-04-06 14:52:58 +0300

    Loading

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -194,89 +194,94 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: const Text('List Products'),
         ),
-        body: FutureBuilder<List<Product>?>(
-          future: ProductService.getProducts(),
-          builder: (_, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
+        body: _isLoading
+            ? const Center(
                 child: CircularProgressIndicator(),
-              );
-            }
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(
-                  snapshot.error.toString(),
-                  style: const TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              );
-            }
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasData) {
-                final List<Product> datas = snapshot.data!;
-
-                return ListView.separated(
-                    itemBuilder: (_, index) {
-                      final product = datas[index];
-
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(product.name!),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(NumberFormat.currency(
-                                locale: 'id-ID',
-                                name: 'Rp ',
-                                decimalDigits: 0,
-                              ).format(product.price)),
-                              Row(
-                                children: [
-                                  // !TOMBOL EDIT
-                                  ElevatedButton.icon(
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor: Colors.amber),
-                                      onPressed: () {
-                                        _showBottomSheet(
-                                            isUpdate: true, product: product);
-                                      },
-                                      icon: Icon(Icons.edit),
-                                      label: Text('Edit')),
-                                  const SizedBox(
-                                    width: 30,
-                                  ),
-                                  // !TOMBOL DELETE
-                                  ElevatedButton.icon(
-                                      style: ElevatedButton.styleFrom(
-                                          backgroundColor: Colors.red),
-                                      onPressed: () {
-                                        _showAlertDialog(product);
-                                      },
-                                      icon: Icon(Icons.delete),
-                                      label: Text('Delete')),
-                                ],
-                              ),
-                            ],
-                          ),
+              )
+            : FutureBuilder<List<Product>?>(
+                future: ProductService.getProducts(),
+                builder: (_, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        snapshot.error.toString(),
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
                         ),
-                      );
-                    },
-                    separatorBuilder: (_, __) {
-                      return const SizedBox(
-                        height: 10,
-                      );
-                    },
-                    itemCount: datas.length);
-              }
-            }
-            return const SizedBox.shrink();
-          },
-        ));
+                      ),
+                    );
+                  }
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      final List<Product> datas = snapshot.data!;
+
+                      return ListView.separated(
+                          itemBuilder: (_, index) {
+                            final product = datas[index];
+
+                            return Card(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(product.name!),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(NumberFormat.currency(
+                                      locale: 'id-ID',
+                                      name: 'Rp ',
+                                      decimalDigits: 0,
+                                    ).format(product.price)),
+                                    Row(
+                                      children: [
+                                        // !TOMBOL EDIT
+                                        ElevatedButton.icon(
+                                            style: ElevatedButton.styleFrom(
+                                                backgroundColor: Colors.amber),
+                                            onPressed: () {
+                                              _showBottomSheet(
+                                                  isUpdate: true,
+                                                  product: product);
+                                            },
+                                            icon: Icon(Icons.edit),
+                                            label: Text('Edit')),
+                                        const SizedBox(
+                                          width: 30,
+                                        ),
+                                        // !TOMBOL DELETE
+                                        ElevatedButton.icon(
+                                            style: ElevatedButton.styleFrom(
+                                                backgroundColor: Colors.red),
+                                            onPressed: () {
+                                              _showAlertDialog(product);
+                                            },
+                                            icon: Icon(Icons.delete),
+                                            label: Text('Delete')),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                          separatorBuilder: (_, __) {
+                            return const SizedBox(
+                              height: 10,
+                            );
+                          },
+                          itemCount: datas.length);
+                    }
+                  }
+                  return const SizedBox.shrink();
+                },
+              ));
   }
 }
